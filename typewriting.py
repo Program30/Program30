785d910910c8e9f6cbdc0d4ca024b653f83d10d5
Program30 <150437395+Program30@users.noreply.github.com>
2023-11-12 09:50:39 +0800

    Update typewriting.py

--- typewriting.py
+++ typewriting.py
@@ -3,13 +3,14 @@ import random as rd
 
 
 
+# After entering the victory conditions, failure conditions, and typing, you need to press the Enter button once you have finished typing
 els = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 rc = 0
 wc = 0
 
 print('This is a typewriting game.')
 print("Let's play this game!")
-print('Please input won condition(how many characters; input a number)')
+print('Please input victory condition(how many characters; input a number)')
 wonc = int(input())
 print('Please input failed condition(how many characters; input a number)')
 failedc = int(input())
