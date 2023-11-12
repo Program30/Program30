99953b6114741dd4d41ea4f893753a8d744d536a
Program30 <150437395+Program30@users.noreply.github.com>
2023-11-12 09:51:17 +0800

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,16 +1,3 @@
-#- 👋 Hi, I’m @Program30
-#-  My name is XiaoYuXuan
-#-  I'am from China
-#-  I'am eleven years old
-#-  I like porgram
-#-  I'm a happy boy.
-#-  I'm use the GitHub first time.
-#-  I'm program is not very good.
-#-  But I hope you can provide me with more opinions to make my work better.
-<!---
-Program30/Program30 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
 import random as rd
 
 
